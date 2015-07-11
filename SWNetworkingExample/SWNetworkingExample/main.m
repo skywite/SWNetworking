7cf3b34af5d6a0498d39c4ec63cbeaa4aed87b92
saman kumara <me@isamankumara.com>
2015-07-12 01:56:00 +0530

    New Example added

--- SWNetworkingExample/SWNetworkingExample/main.m
+++ SWNetworkingExample/SWNetworkingExample/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  Example
+//  SWNetworkingExample
 //
-//  Created by Saman Kumara on 7/6/15.
+//  Created by Saman Kumara on 7/12/15.
 //  Copyright (c) 2015 Saman Kumara. All rights reserved.
 //
 
--- SWNetworkingExample/SWNetworkingExampleTests/SWNetworkingExampleTests.m
+++ SWNetworkingExample/SWNetworkingExampleTests/SWNetworkingExampleTests.m
@@ -1,19 +1,19 @@
 //
-//  ExampleTests.m
-//  ExampleTests
+//  SWNetworkingExampleTests.m
+//  SWNetworkingExampleTests
 //
-//  Created by Saman Kumara on 7/6/15.
+//  Created by Saman Kumara on 7/12/15.
 //  Copyright (c) 2015 Saman Kumara. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface ExampleTests : XCTestCase
+@interface SWNetworkingExampleTests : XCTestCase
 
 @end
 
-@implementation ExampleTests
+@implementation SWNetworkingExampleTests
 
 - (void)setUp {
     [super setUp];
